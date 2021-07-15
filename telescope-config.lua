ec72d3402134e53577b8d04efd4cd1d36b263825
grevyarlesp <tr4nquoc@gmail.com>
2021-07-15 15:03:29 +0700

    telescope, signify

--- telescope-config.lua
+++ telescope-config.lua
@@ -1,53 +1,33 @@
--- require('telescope').setup{
---   defaults = {
---     vimgrep_arguments = {
---       'rg',
---       '--color=never',
---       '--no-heading',
---       '--with-filename',
---       '--line-number',
---       '--column',
---       '--smart-case'
---     },
---     prompt_position = "bottom",
---     prompt_prefix = "> ",
---     selection_caret = "> ",
---     entry_prefix = "  ",
---     initial_mode = "insert",
---     selection_strategy = "reset",
---     sorting_strategy = "descending",
---     layout_strategy = "horizontal",
---     layout_defaults = {
---       horizontal = {
---         mirror = false,
---       },
---       vertical = {
---         mirror = false,
---       },
---     },
---     file_sorter =  require'telescope.sorters'.get_fuzzy_file,
---     file_ignore_patterns = {},
---     generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
---     shorten_path = true,
---     winblend = 0,
---     width = 0.8,
---     preview_cutoff = 120,
---     results_height = 1,
---     results_width = 0.8,
---     border = {},
---     borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
---     -- borderchars =  { '─', '│', '─', '│', '┌', '┐', '┘', '└'},
---     color_devicons = true,
---     use_less = true,
---     set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
---     file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
---     grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
---     qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+require("telescope").setup {
+  defaults = {
+    -- Your defaults config goes in here
+  },
+  pickers = {
+    -- Your special builtin config goes in here
+    buffers = {
+      sort_lastused = true,
+      theme = "dropdown",
+      previewer = false,
+      mappings = {
+        i = {
+          ["<c-d>"] = require("telescope.actions").delete_buffer,
+          -- Right hand side can also be the name of the action as a string
+          ["<c-d>"] = "delete_buffer",
+        },
+        n = {
+          ["<c-d>"] = require("telescope.actions").delete_buffer,
+        }
+      }
+    },
+    find_files = {
+      theme = "dropdown"
+    }
+  },
+  extensions = {
+    -- Your extension config goes in here
+  }
+}
 
---     -- Developer configurations: Not meant for general override
---     buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
---   }
--- }
 vim.cmd(
 [[
 " Find files using Telescope command-line sugar.
@@ -57,5 +37,13 @@ nnoremap <leader>fg <cmd>Telescope live_grep<cr>
 nnoremap <leader>fb <cmd>Telescope buffers<cr>
 nnoremap <leader>fh <cmd>Telescope help_tags<cr>
 nnoremap <leader>fr <cmd>Telescope registers<cr>
+
+nnoremap <leader>fc <cmd>lua require'telescope.builtin'.git_bcommits()<cr>
+nnoremap <leader>fs <cmd>lua require'telescope.builtin'.git_status()<cr>
+" nnoremap <leader>fs <cmd>lua require'telescope.builtin'.git_status()<cr>
+
+nnoremap <leader>fz <cmd>lua require'telescope.builtin'.current_buffer_fuzzy_find()<cr>
+nnoremap <leader>ft <cmd>lua require'telescope.builtin'.treesitter()<cr>
+
 ]])
 
