64d00b5d6ab578c72e6f3f265728119fcf4cacc4
grevyarlesp <tr4nquoc@gmail.com>
2021-07-17 00:12:38 +0700

    Fix slowness TS

--- nvim-treesitter-config.lua
+++ nvim-treesitter-config.lua
@@ -3,6 +3,6 @@ require'nvim-treesitter.configs'.setup {
   ignore_install = { "javascript" }, -- List of parsers to ignore installing
   highlight = {
     enable = true              -- false will disable the whole extension
-    -- disable = { "c", "rust" },  -- list of language that will be disabled
+    disable = { "c", "rust", "python"},  -- list of language that will be disabled
   },
 }
