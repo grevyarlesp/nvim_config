005aee345ddac5923a1e5e72ecc353711b63db65
grevyarlesp <tr4nquoc@gmail.com>
2021-07-19 16:09:35 +0700

    lualine

---
+++ lua/core/lualine.lua
@@ -0,0 +1,30 @@
+
+require('lualine').setup{
+    options = {
+        theme = 'tokyonight',
+        -- section_separators = {'', ''},
+        section_separators = {'' , ''},
+        -- section_separators = {'', ''},
+        component_separators = {'', ''},
+        disabled_filetypes = {},
+        icons_enabled = true,
+    },
+
+    sections = {
+        lualine_a = { {'mode', upper = false} },
+        lualine_b = { {'FugitiveHead', icon = ''} },
+        lualine_c = { {'filename', file_status = true}},
+        lualine_x = { {'diagnostics', sources={'nvim_lsp'}}, 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location'  },
+    },
+    inactive_sections = {
+        lualine_a = {  },
+        lualine_b = {  },
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {  },
+        lualine_z = {   }
+    },
+}
+
