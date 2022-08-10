95c3153d5a457eda85ee56de5dc1d7562cae30a9
grevyarlesp <tr4nquoc@gmail.com>
2022-08-10 15:29:54 +0700

    more metals

--- lua/lsp/metals.lua
+++ lua/lsp/metals.lua
@@ -16,8 +16,96 @@ metals_config.settings = {
 -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 metals_config.capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+
+
 local api = vim.api
 local cmd = vim.cmd
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+----------------------------------
+-- OPTIONS -----------------------
+----------------------------------
+-- global
+vim.opt_global.completeopt = { "menuone", "noinsert", "noselect" }
+vim.opt_global.shortmess:remove("F"):append("c")
+
+-- LSP mappings
+map("n", "gD", "<cmd>lua vim.lsp.buf.definition()<CR>")
+map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+-- map("n", "gds", "<cmd>lua vim.lsp.buf.document_symbol()<CR>")
+map("n", "gds", "<cmd>lua require(\"telescope.builtin\").lsp_document_symbols()<CR>")
+map("n", "gws", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>")
+map("n", "<leader>cl", [[<cmd>lua vim.lsp.codelens.run()<CR>]])
+map("n", "<leader>sh", [[<cmd>lua vim.lsp.buf.signature_help()<CR>]])
+map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+map("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>")
+map("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+map("n", "<leader>ws", '<cmd>lua require"metals".hover_worksheet()<CR>')
+map("n", "<leader>aa", [[<cmd>lua vim.diagnostic.setqflist()<CR>]]) -- all workspace diagnostics
+map("n", "<leader>ae", [[<cmd>lua vim.diagnostic.setqflist({severity = "E"})<CR>]]) -- all workspace errors
+map("n", "<leader>aw", [[<cmd>lua vim.diagnostic.setqflist({severity = "W"})<CR>]]) -- all workspace warnings
+map("n", "<leader>d", "<cmd>lua vim.diagnostic.setloclist()<CR>") -- buffer diagnostics only
+map("n", "[c", "<cmd>lua vim.diagnostic.goto_prev { wrap = false }<CR>")
+map("n", "]c", "<cmd>lua vim.diagnostic.goto_next { wrap = false }<CR>")
+
+-- Example mappings for usage with nvim-dap. If you don't use that, you can
+-- skip these
+map("n", "<leader>dc", [[<cmd>lua require"dap".continue()<CR>]])
+map("n", "<leader>dr", [[<cmd>lua require"dap".repl.toggle()<CR>]])
+map("n", "<leader>dK", [[<cmd>lua require"dap.ui.widgets".hover()<CR>]])
+map("n", "<leader>dt", [[<cmd>lua require"dap".toggle_breakpoint()<CR>]])
+map("n", "<leader>dso", [[<cmd>lua require"dap".step_over()<CR>]])
+map("n", "<leader>dsi", [[<cmd>lua require"dap".step_into()<CR>]])
+map("n", "<leader>dl", [[<cmd>lua require"dap".run_last()<CR>]])
+
+map("n", "<leader>ts", [[<cmd>lua require("telescope").extensions.metals.commands()<CR>]])
+
+-- completion related settings
+-- This is similiar to what I use
+local cmp = require("cmp")
+cmp.setup({
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "vsnip" },
+  },
+  snippet = {
+    expand = function(args)
+      -- Comes from vsnip
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    -- None of this made sense to me when first looking into this since there
+    -- is no vim docs, but you can't have select = true here _unless_ you are
+    -- also using the snippet stuff. So keep in mind that if you remove
+    -- snippets you need to remove this select
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    -- I use tabs... some say you should stick to ins-completion but this is just here as an example
+    ["<Tab>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end,
+    ["<S-Tab>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end,
+  }),
+})
+
 -- Autocmd that will actually be in charging of starting the whole thing
 local nvim_metals_group = api.nvim_create_augroup("nvim-metals", { clear = true })
 api.nvim_create_autocmd("FileType", {
@@ -30,3 +118,5 @@ api.nvim_create_autocmd("FileType", {
   end,
   group = nvim_metals_group,
 })
+
+
